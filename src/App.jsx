9b475df503a804f5230a47d314718ca53fdd7bb6
Anbuselvan04 <adsanbuselvan@gmail.com>
2024-07-05 19:46:58 +0530

    Success

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import GetData from './components/getData'
+import './App.css'
+
+function App() {
+  return (
+    <React.Fragment>
+      <GetData />
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://Anbuselvan04.github.io/api-activity'
+})
